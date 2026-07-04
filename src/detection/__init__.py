97032371dc436796a861045b65757639c59ebc9b
ahmedsharaf19 <engsharaf192002@gmail.com>
2026-07-04 15:31:49 +0300

    Expose TeamAssigner from src/detection/__init__.py

--- src/detection/__init__.py
+++ src/detection/__init__.py
@@ -1,4 +1,6 @@
 from .train import train_yolo
 from .track import FootballTracker
+from .team_assigner import TeamAssigner
 
-__all__ = ["train_yolo", "FootballTracker"]
+__all__ = ["train_yolo", "FootballTracker", "TeamAssigner"]
+
